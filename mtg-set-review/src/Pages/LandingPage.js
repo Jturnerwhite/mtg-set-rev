a8e4d354982b32d376998252ca340db79f178c8e
Jordan White <jordan@volanosoftware.com>
2021-09-17 14:53:16 -0500

    Routing

---
+++ mtg-set-review/src/Pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default class LandingPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>A page!</h1>
+            </div>
+        );
+    }
+}
