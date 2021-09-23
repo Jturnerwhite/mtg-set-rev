531a45956b3fc8731a68b133d1b43130d7788320
Jordan White <jordan@volanosoftware.com>
2021-09-23 13:27:29 -0500

    slider

--- mtg-set-review/src/Components/rating-slider/rating-slider.js
+++ mtg-set-review/src/Components/rating-slider/rating-slider.js
@@ -14,16 +14,25 @@ function RatingSlider(props) {
         updateSliderValue(event.target.value);
     }
 
+    function valueClick(val) {
+        updateSliderValue(val);
+    }
+
     return (
         <div className="slider-container">
             <input className="slider" type="range" min="0" max="10" value={sliderValue} onChange={handleChange} />
             <ul className="rating">
-                <li>0.0</li>
-                <li>1.0</li>
-                <li>2.0</li>
-                <li>3.0</li>
-                <li>4.0</li>
-                <li>5.0</li>
+                <li onClick={() => { valueClick(0) }}>0</li>
+                <li onClick={() => { valueClick(1) }}>0.5</li>
+                <li onClick={() => { valueClick(2) }}>1</li>
+                <li onClick={() => { valueClick(3) }}>1.5</li>
+                <li onClick={() => { valueClick(4) }}>2</li>
+                <li onClick={() => { valueClick(5) }}>2.5</li>
+                <li onClick={() => { valueClick(6) }}>3</li>
+                <li onClick={() => { valueClick(7) }}>3.5</li>
+                <li onClick={() => { valueClick(8) }}>4</li>
+                <li onClick={() => { valueClick(9) }}>4.5</li>
+                <li onClick={() => { valueClick(10) }}>5</li>
             </ul>
         </div>
     );
